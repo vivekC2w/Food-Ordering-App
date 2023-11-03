46a8d83cde3c8fbeaa5fb7302b5195f01d5ac566
vivekC2w <68582416+vivekC2w@users.noreply.github.com>
2023-11-04 04:39:45 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,4 +5,4 @@ export const LOGO_URL =
   "https://dynamic.brandcrowd.com/asset/logo/53703663-f2ea-4e81-8b89-44965b073800/logo-search-grid-1x?logoTemplateVersion=1&v=637781767853000000";
 
 export const MENU_API =
-  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.494033&lng=73.898427&restaurantId=";
+  "https://corsproxy.io/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.494033&lng=73.898427&restaurantId=";
